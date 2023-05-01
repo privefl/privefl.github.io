036babfd81bc231c1bbc557a4168ada768c8444e
Florian Privé <florian.prive.21@gmail.com>
2023-05-01 10:46:02 +0200

    conso

---
+++ conso-pompe-piscine.R
@@ -0,0 +1,7 @@
+vitesse <- c(0, 10:20 * 100, 2230, 2480, 2810, 3140)
+conso <- c(0, 142, 157, 178, 200, 225, 254, 293, 324, 365, 411, 464, 607, 799, 1129, 1576)
+
+library(ggplot2)
+theme_set(theme_bw(16))
+qplot(vitesse, conso) + geom_line()
+qplot(vitesse, conso / vitesse) + geom_line() + ylim(0, NA)
